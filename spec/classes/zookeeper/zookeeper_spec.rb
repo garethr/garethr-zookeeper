4d7e2b7f70f3d28835cc8fd2b90e91b6aec0e708
Kyle Anderson <kyle@xkyle.com>
2013-10-26 14:16:12 -0700

    Added tests for new external_repo feature

--- spec/classes/zookeeper/zookeeper_spec.rb
+++ spec/classes/zookeeper/zookeeper_spec.rb
@@ -16,4 +16,18 @@ describe 'zookeeper', :type => :class do
     it { should contain_service('zookeeper-server').with_ensure('running').with_enable('true') }
   end
 
+  context 'When asked not to include an external repo' do
+    let(:facts) { {:osfamily => 'Debian', :lsbdistcodename => 'precise'} }
+    let(:params) { {:use_external_repo => false} }
+    it { should_not include_class('wget') }
+    it { should_not include_class('java') }
+    it { should_not contain_package('cloudera_repo') }
+  end
+
+  context 'When given a bogus falue for use_external_repo' do
+    let(:facts) { {:osfamily => 'Debian', :lsbdistcodename => 'precise'} }
+    let(:params) { {:use_external_repo => 'false'} }
+    it { expect { subject }.to raise_error() }
+  end
+
 end
