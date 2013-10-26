bb95cfccee16fbb4d8ca27aeafebe242afd6816a
Kyle Anderson <kyle@xkyle.com>
2013-10-26 14:54:37 -0700

    Adjusted install to account for difference in package names

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,32 +4,31 @@ class zookeeper::install {
   if $::zookeeper::use_external_repo == true {
     include wget
     include java
-  
     wget::fetch { 'download_cloudera_repo':
       source      => 'http://archive.cloudera.com/cdh4/one-click-install/precise/amd64/cdh4-repository_1.0_all.deb',
       destination => '/usr/local/src/cdh4-repository_1.0_all.deb',
       before      => Package['cloudera_repo'],
     }
-  
     package { 'cloudera_repo':
       ensure   => installed,
       source   => '/usr/local/src/cdh4-repository_1.0_all.deb',
       provider => dpkg,
       notify   => Exec['apt-update-cloudera'];
     }
-  
     exec { 'apt-update-cloudera':
       command     => '/usr/bin/apt-get update',
       refreshonly => true,
       before      => [ Package['zookeeper'], Package['zookeeper-server'] ],
     }
-  }
+    package { [ 'zookeeper', 'zookeeper-server', ]:
+      ensure  => installed,
+    }
 
-  package { [
-    'zookeeper',
-    'zookeeper-server',
-  ]:
-    ensure  => installed,
+  } else {
+    # Use stock packages
+    package { [ 'zookeeper', 'zookeeperd' ]:
+      ensure => installed,
+    }
   }
 
 }
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,5 +1,11 @@
 class zookeeper::service {
-  service { 'zookeeper-server':
+  if $::zookeeper::use_external_repo == true {
+    $service_name = 'zookeeper-server'
+  } else {
+    $service_name = 'zookeeper'
+  }
+
+  service { $service_name:
     ensure     => running,
     enable     => true,
     hasstatus  => true,
