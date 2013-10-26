59d853992155497383014ce15835631490620789
Kyle Anderson <kyle@xkyle.com>
2013-10-26 14:32:25 -0700

    Fixed implcit path required for service init

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,5 +2,6 @@ class zookeeper::config {
   exec { 'initialize_zookeeper':
     command => 'service zookeeper-service init',
     creates => '/var/lib/zookeeper/version-2',
+    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
   }
 }
